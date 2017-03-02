f62cc15546dfbcdda3cbf5c497f4393c10f45601
Gerardo Galvez <gerardogv96@hotmail.com>
2017-03-01 23:15:58 -0600

    Load half-moons dataset and add noise

---
+++ dbscan.py
@@ -0,0 +1,19 @@
+from sklearn.datasets import make_moons
+import numpy as np
+
+# Add outliers to the half-moons
+def addNoise(X, y, noiseLevel = 0.01):
+	# Amount of noisy points
+	amountNoise = int(noiseLevel * len(y))
+
+	# Select random points to add noise
+	index = np.random.choice(len(X), size = amountNoise)
+
+	# Add noise
+	noise = np.random.random((amountNoise, len(X[0]))) - 0.5
+	X[index, :] += noise
+
+def main():
+	# moons_X: Data, moon_y: Labels
+	moons_X, moon_y = make_moons(n_samples = 2000)
+	addNoise(X, y)
